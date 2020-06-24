670c3462f4f0e8a767ed2f667f12a81073bb57ba
Nomi Ringach <5738735+nbingo@users.noreply.github.com>
2020-06-23 17:09:25 -0700

    Revert "Revert "Basic dendrogram working, but not correct distance function.""
    This reverts commit 0dc1484e30c750ae03b937111ea906695c450018.

--- BasicDendrogram.py
+++ BasicDendrogram.py
@@ -56,7 +56,8 @@ def read_preprocess_data():
     data_human = data_human.div(data_human.mean(axis=1).values, axis=0).add_prefix('H_').transpose()
 
     # Concatenate all the data
-    data_all = pd.concat([data_mouse, data_chicken, data_human])
+    # data_all = pd.concat([data_mouse, data_chicken, data_human])
+    data_all = pd.concat([data_mouse, data_chicken])
 
     return data_all
 
@@ -64,6 +65,9 @@ def read_preprocess_data():
 if __name__ == '__main__':
     data = read_preprocess_data()
 
-    model = AgglomerativeClustering(n_clusters=None, distance_threshold=0)
+    model = AgglomerativeClustering(linkage='complete', n_clusters=None, distance_threshold=0)
     model.fit(data.to_numpy())
 
+    plt.title('Hierarchical clustering')
+    plot_dendrogram(model, truncate_mode='level')
+    plt.show()
