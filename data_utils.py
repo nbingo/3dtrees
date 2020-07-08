6c7c5e85606bd32a7011b2a0b4907d6a84f18322
Nomi Ringach <5738735+nbingo@users.noreply.github.com>
2020-07-08 12:04:25 -0700

    Now properly filtering for p-value and logFC change

--- data_utils.py
+++ data_utils.py
@@ -2,6 +2,9 @@ import pandas as pd
 import numpy as np
 from typing import List, Optional
 
+P_VAL_ADJ_THRESH = 0.01
+AVG_LOG_FC_THRESH = 2
+
 
 def read_data(species: List[str], deg_species: Optional[List[str]] = None, preprocess: Optional[bool] = True):
     if deg_species is None:
@@ -11,6 +14,11 @@ def read_data(species: List[str], deg_species: Optional[List[str]] = None, prepr
     species_data = [pd.read_csv(f'TranscriptomeData/Nomi_{s}allaverage.csv', header=0, index_col=0) for s in species]
 
     if preprocess:
+        # Filter for adjusted p-value and logFC
+        for i in range(len(species_degs)):
+            species_degs[i] = species_degs[i].loc[(species_degs[i]['p_val_adj'] < P_VAL_ADJ_THRESH) &
+                                                  (species_degs[i]['avg_logFC'] > AVG_LOG_FC_THRESH)]
+
         # Find the common DEGs
         common_genes = species_degs[0]['gene']
         for deg in species_degs:
