432161dfa1283439fcbee5163dfd1b7e7ce6532a
Nomi Ringach <5738735+nbingo@users.noreply.github.com>
2020-07-30 22:31:13 -0700

    Updated output for experiments. Graphs not saving for some reason...

--- BasicBatchDendrogram3D.py
+++ BasicBatchDendrogram3D.py
@@ -10,15 +10,18 @@ if __name__ == '__main__':
         cell_type_affinity=[spearmanr_connectivity],
         linkage_cell=['complete'],
         linkage_region=['homolog_avg'],
-        max_region_diff=[0, 1],
-        region_dist_scale=np.arange(0.7, 1, 0.01),
+        max_region_diff=[1],
+        region_dist_scale=np.arange(0.5, 1.5, 0.005),
         verbose=False
     )
 
     agglomerate.agglomerate(data)
     best_agglomerators = agglomerate.get_best_agglomerators()
     pd.options.display.width = 0
-    for metric in TREE_SCORE_OPTIONS:
+    # for metric in TREE_SCORE_OPTIONS:
+    for metric in ['BME']:
         print(f'Best {metric} score: {best_agglomerators[metric][0]}\n'
               f'Agglomerator: {best_agglomerators[metric][1]}\n'
+              f'region_dist_scale: {best_agglomerators[metric][1].region_dist_scale}\n'
               f'Tree:\n{best_agglomerators[metric][1].linkage_mat_readable}\n\n')
+        best_agglomerators[metric][1].view_tree3d()
