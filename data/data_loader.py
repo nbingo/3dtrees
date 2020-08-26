1c0e4250638fd11fc32bf84f88ffa80f3acc3e61
nbingo <5738735+nbingo@users.noreply.github.com>
2020-08-26 11:12:57 -0700

    Clearer return types and namespaces.

--- data/data_loader.py
+++ data/data_loader.py
@@ -1,21 +1,21 @@
-from typing import List, Optional
+from typing import Sequence, Optional
 from abc import abstractmethod
-from collections.abc import Sequence
+from collections import abc
 import numpy as np
 
 
-class DataLoader(Sequence):
+class DataLoader(abc.Sequence):
 
     def __init__(self, ct_axis_mask: Optional[np.array] = None, r_axis_mask: Optional[np.array] = None):
         self.ct_axis_mask = ct_axis_mask
         self.r_axis_mask = r_axis_mask
 
     @abstractmethod
-    def get_names(self) -> List[str]:
+    def get_names(self) -> Sequence[str]:
         pass
 
     @abstractmethod
-    def get_corresponding_region_names(self) -> List[str]:
+    def get_corresponding_region_names(self) -> Sequence[str]:
         pass
 
     @abstractmethod
